65853f394269ff3f5fc3fcf1dda1bccd01ac9b8d
Rggyg <162576203+Rggyg@users.noreply.github.com>
2024-07-15 23:44:09 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-/*SIDE BAR*/
-const body = document.querySelector("body")
-const sideBar = body.querySelector("nav")
-const toggle = body.querySelector(".hide")
-const info = body.querySelector("section")
-toggle.addEventListener("click",()=>{
-    sideBar.classList.toggle("hide");
-    info.classList.toggle("mini");
-})
